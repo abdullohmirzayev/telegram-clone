4bc2cbb9bc8a459a4ff1f597a9a0c7f8836c8a6a
Abdulloh <abdullohmirzayev7570@gmail.com>
2024-11-20 23:57:32 +0500

    added Profile settings

--- lib/validation.ts
+++ lib/validation.ts
@@ -1,12 +1,18 @@
-'use client'
-
 import { z } from 'zod'
 
 export const emailSchema = z.object({
 	email: z
 		.string()
-		.email({ message: 'Invalid email adress, please check and try again.' }),
+		.email({ message: 'Invalid email address, please check and try again.' }),
 })
+
+export const oldEmailSchema = z
+	.object({
+		oldEmail: z
+			.string()
+			.email({ message: 'Invalid email address, please check and try again.' }),
+	})
+	.merge(emailSchema)
 
 export const otpSchema = z
 	.object({
@@ -20,3 +26,16 @@ export const messageSchema = z.object({
 	text: z.string().min(1, { message: 'Message cannot be empty.' }),
 	image: z.string().optional(),
 })
+
+export const profileSchema = z.object({
+	firstName: z.string().min(2),
+	lastName: z.string().optional(),
+	bio: z.string().optional(),
+})
+
+export const confirmTextSchema = z
+	.object({ confirmText: z.string() })
+	.refine(data => data.confirmText === 'DELETE', {
+		message: 'You must type DELETE to confirm.',
+		path: ['confirmText'],
+	})
